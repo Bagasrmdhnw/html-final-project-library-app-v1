60e9ee3ef21bb8f127000f96b291fcbcc7295897
Ari Bagas Ramadhan Wibowo <90238274+Bagasrmdhnw@users.noreply.github.com>
2023-05-21 22:26:25 +0700

    baruinit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Ari Bagas Ramadhan Wibowo'; // TODO: replace this
+
+const studentId = 'FE5375505'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://polite-buttercream-a4af83.netlify.app/'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };

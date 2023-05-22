882f5966167bc687d17fa8d7640dd797d3d76e36
Ari Bagas Ramadhan Wibowo <90238274+Bagasrmdhnw@users.noreply.github.com>
2023-05-22 10:36:47 +0700

    updatefix

--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Ari Bagas Ramadhan Wibowo'; // TODO: replace this
 
 const studentId = 'FE5375505'; // TODO: replace this
 
-const NetlifyDeployUrl = 'https://polite-buttercream-a4af83.netlify.app/'; // TODO: replace this
+const NetlifyDeployUrl = 'https://resonant-hamster-c88407.netlify.app'; // TODO: replace this
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
